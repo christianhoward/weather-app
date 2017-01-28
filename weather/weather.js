e556e0f3e927bfee5c1240a7a4041b576f07315b
christianhoward <christian.john.howard@gmail.com>
2017-01-28 12:21:30 -0500

    Updated weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 var getWeather = (lat, lng, callback) => {
   request({
-    url: `https://api.darksky.net/forecast/e7f5d1915cafa1e1a6134590a4fc567d/${lat},${lng}`,
+    url: `https://api.darksky.net/forecast/[key]/${lat},${lng}`,
     json: true
   }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
